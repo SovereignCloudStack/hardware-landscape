63059e256f331b07edf0e1dac7f1782f6b6a8796
Marc Schöchlin <schoechlin@osb-alliance.com>
2024-07-02 13:33:16 +0200

    fix
    Signed-off-by: Marc Schöchlin <schoechlin@osb-alliance.com>

--- environments/custom/roles/scs-landscape-nodes/files/scripts/scs_add_netfilter_rules.sh
+++ environments/custom/roles/scs-landscape-nodes/files/scripts/scs_add_netfilter_rules.sh
@@ -1,32 +1,40 @@
 #!/bin/bash
 
-RULE_EXISTS="false"
-OUTDATED_RULES_HANDLES=""
-RULE_MARKER="SCS-MASQUERADING"
+add_rule(){
+   local POS_WANTED="$1"
+   local RULE="$2"
+   local RULE_EXISTS="false"
+   local OUTDATED_RULES_HANDLES=""
+   local RULE_MARKER="SCS-MASQ"
 
-while read RULE_HANDL POS;
-do
-   if ( [ "$POS" = "0" ] && [ "$1" != "force" ] );then
-      RULE_EXISTS="true"
+   while read RULE_HANDL POS;
+   do
+      if ( [ "$POS" = "${POS_WANTED}" ] && [ "$1" != "force" ] );then
+         RULE_EXISTS="true"
+      else
+         OUTDATED_RULES_HANDLES="$RULE_HANDL $OUTDATED_RULES_HANDLES"
+      fi
+   done < <(nft -a list chain nat POSTROUTING|awk -v "marker=$RULE_MARKER" '
+      BEGIN{ pos=-1; }
+      $0 ~ marker {printf("%s %s\n",$(NF),pos); }
+      /# handle [0-9][0-9]*/{ pos++; }'
+   )
+
+   if ( [ "$FORCE" = "force" ] || [ "$RULE_EXISTS" != "true" ] );then
+      echo "adding rule to position ${POS_WANTED}"|logger -s -t scs_add_netfilter_rules
+      nft insert rule "ip nat POSTROUTING position ${POS_WANTED} limit rate 30/minute log prefix $RULE_MARKER $RULE"
    else
-      OUTDATED_RULES_HANDLES="$RULE_HANDL $OUTDATED_RULES_HANDLES"
+      echo "rule already on position ${POS_WANTED}"|logger -s -t scs_add_netfilter_rules
    fi
-done < <(nft -a list chain nat POSTROUTING|awk -v "marker=$RULE_MARKER" '
-   BEGIN{ pos=-1; }
-   $0 ~ marker {printf("%s %s\n",$(NF),pos); }
-   /# handle [0-9][0-9]*/{ pos++; }'
-)
-
-if [ "$RULE_EXISTS" != "true" ];then
-   echo "adding rule to position 0"|logger -s -t scs_add_netfilter_rules
-   nft insert rule ip nat POSTROUTING position 0 log prefix $RULE_MARKER ip saddr "{ 172.31.100.0/23, 10.10.1.0/24 }" ip daddr 10.10.21.0/24 snat to 10.10.21.10
-else
-   echo "rule already on position 0"|logger -s -t scs_add_netfilter_rules
-fi
 
 
-for RULE_HANDL in $OUTDATED_RULES_HANDLES;
-do
-   echo "removing handle $RULE_HANDL"|logger -s -t scs_add_netfilter_rules
-   nft delete rule nat POSTROUTING handle $RULE_HANDL
-done
+   for RULE_HANDL in $OUTDATED_RULES_HANDLES;
+   do
+      echo "removing handle $RULE_HANDL"|logger -s -t scs_add_netfilter_rules
+      nft delete rule nat POSTROUTING handle $RULE_HANDL
+   done
+}
+
+FORCE="${1:-add}"
+add_rule 0 'ip saddr { 172.31.100.0/23, 10.10.1.0/24 } ip daddr 10.10.21.0/24 snat to 10.10.21.10'
+add_rule 0 'ip saddr { 172.31.100.0/23, 10.10.1.0/24 } ip daddr 10.10.23.0/24 snat to 10.10.23.254'
