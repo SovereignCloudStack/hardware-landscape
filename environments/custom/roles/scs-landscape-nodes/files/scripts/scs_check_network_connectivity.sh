c6a180917cbefcf9a63bc7530bf6cd5865c13917
Marc Schöchlin <schoechlin@osb-alliance.com>
2024-07-30 13:02:29 +0200

    add
    Signed-off-by: Marc Schöchlin <schoechlin@osb-alliance.com>

--- environments/custom/roles/scs-landscape-nodes/files/scripts/scs_check_network_connectivity.sh
+++ environments/custom/roles/scs-landscape-nodes/files/scripts/scs_check_network_connectivity.sh
@@ -4,7 +4,7 @@
 failed=""
 grep -P "st01-(stor|mgmt|comp|ctl)-r\d+-u\d+" /etc/hosts|awk '{print $(NF)}'|sort|while read node
 do
-   ping -q -c 2 $node
+   ping -q -c ${1:-2} $node
    if [ "$?" != "0" ];then
       failed="$node $failed"
    fi
