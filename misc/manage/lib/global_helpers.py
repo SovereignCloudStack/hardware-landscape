f9d5c952ba81dc2c609cf78eecb5f1db5c4db4e4
Marc Schöchlin <schoechlin@osb-alliance.com>
2024-07-31 17:15:32 +0200

    add python script
    Signed-off-by: Marc Schöchlin <schoechlin@osb-alliance.com>

--- misc/manage/lib/global_helpers.py
+++ misc/manage/lib/global_helpers.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import re
 from datetime import datetime, timezone
 from typing import Tuple
 
@@ -61,3 +62,15 @@ def ask_for_confirmation(prompt="Are you sure?"):
             return False
         else:
             print("Please respond with 'yes' or 'no'.")
+
+
+def generate_strings(expression: str):
+    match = re.match(r'([^\{]*)\{(\d+)\.\.(\d+)\}([^\{]*)', expression)
+    results = []
+    if match:
+        prefix, start, end, suffix = match.groups()
+        start, end = int(start), int(end)
+        results = [f"{prefix}{num}{suffix}" for num in range(start, end + 1)]
+    else:
+        results.append(expression)
+    return results
