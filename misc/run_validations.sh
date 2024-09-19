82e1776588714f8acf91b56ba5ef8bb2354ce6d2
Marc Schoechlin <schoechlin@osb-alliance.com>
2024-09-19 08:51:31 +0000

    log validations
    Signed-off-by: Marc Schoechlin <schoechlin@osb-alliance.com>

--- misc/run_validations.sh
+++ misc/run_validations.sh
@@ -4,7 +4,9 @@
 # - some validations are ignored, see https://github.com/osism/python-osism/pull/1054
 # - There is no "all" validations target
 
-echo "Logging validations to /tmp/validate.log"
+LOGFILE="/var/log/validations-$(date --date="today" "+%Y-%m-%d_%H-%M-%S")"
+
+echo "Logging validations to $LOGFILE"
 (
 FAILED=""
 while read val; do 
@@ -17,4 +19,4 @@ while read val; do
 done < <(osism validate 2>&1|grep barbican|tr '{,}' '\n\n\n'|grep -v -P "(mariadb-backup|mariadb-recovery|senlin-config)"|grep "[a-z]")
 
 echo "FAILED VALIDATIONS: ${FAILED:-none}"
-) 2>&1 |tee /tmp/validate.log
+) 2>&1 |sudo tee $LOGFILE
