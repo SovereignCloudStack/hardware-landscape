cb8bfa8d95e10759e750e9ff1f885df36e392e8d
Marc Schöchlin <schoechlin@osb-alliance.com>
2024-09-25 13:16:22 +0200

    upgrade to release 8.0.0 (#44)
    - Upgrade to release 8 (reinstall openstack)
    - Install Scaphandre and Promentheus
    - Add preinstall/postinstall validation scrips
    -  Specify minimal kernel version
    - Unsuccessfuly k3s experiments
    - Create a systemd unit to establish NFT rules (add logger commands for better analytics)
    - Minimal SSHD hardening
    - Install HWE kernel
    - A minimal helper script for running all OSISM validations
    
    ---------
    
    
    Signed-off-by: Marc Schöchlin <schoechlin@osb-alliance.com>

--- environments/custom/roles/scs-landscape-nodes/files/scripts/scs_add_nftables_rules.sh
+++ environments/custom/roles/scs-landscape-nodes/files/scripts/scs_add_nftables_rules.sh
@@ -9,13 +9,17 @@ if [ "$1" = "log" ];then
 fi
 
 mkdir -p /var/backups/nft
-nft list ruleset > /var/backups/nft/nft-`date --date="today" "+%Y-%m-%d_%H-%M-%S"`.nft
+BACKUP_FILE="/var/backups/nft/nft-`date --date="today" "+%Y-%m-%d_%H-%M-%S"`.nft"
+
+echo "create backup of old rules: $BACKUP_FILE"|logger -s -t scs_add_nftables_rules
+nft list ruleset > $BACKUP_FILE
 find /var/backups/nft -mtime +30 -type f -delete
 
 ZONE1_IPV4="$(ip -json addr ls | jq -r '.[] | .addr_info[] | select(.local | startswith("10.10.21")) | .local')"
 MGMT_IPV4="$(ip -json addr ls | jq -r '.[] | .addr_info[] | select(.local | startswith("10.10.23")) | .local')"
 MGMTP2P_IPV4="$(ip -json addr ls | jq -r '.[] | .addr_info[] | select(.local | startswith("10.10.22")) | .local')"
 
+echo "add standard rules"|logger -s -t scs_add_nftables_rules
 set -x
 nft "insert rule ip nat POSTROUTING position 0 $LOG ip saddr 172.31.100.0/23 ip daddr 10.10.21.0/24 snat to $ZONE1_IPV4 comment SCS-MASQ"
 nft "insert rule ip nat POSTROUTING position 0 $LOG ip saddr 10.10.1.0/24 ip daddr 10.10.21.0/24 snat to $ZONE1_IPV4 comment SCS-MASQ"
@@ -26,13 +30,14 @@ set +x
 
 if [ -n "$MGMTP2P_IPV4" ];then
 	set -x
+   echo "add MGMTP2P_IPV4 rules"|logger -s -t scs_add_nftables_rules
 	nft "insert rule ip nat POSTROUTING position 0 $LOG ip saddr 10.10.1.0/24 ip daddr 10.10.22.0/24 snat to $MGMTP2P_IPV4 comment SCS-MASQ"
 	nft "insert rule ip nat POSTROUTING position 0 $LOG ip saddr 172.31.100.0/23 ip daddr 10.10.22.0/24 snat $MGMTP2P_IPV4 comment SCS-MASQ"
 	set +x
 fi
 
 for rule in $OLD_RULES; do
-   echo "remove rule with handle $rule"
+   echo "remove rule with handle $rule"|logger -s -t scs_add_nftables_rules
    nft delete rule nat POSTROUTING handle $rule
 done
 
